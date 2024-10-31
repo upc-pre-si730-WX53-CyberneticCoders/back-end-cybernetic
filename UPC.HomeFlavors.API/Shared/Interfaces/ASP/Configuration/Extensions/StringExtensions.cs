ac8e78e5332a9848cd752e4ef234be680ea2550a
Yair Aru Acevedo <u202125984@upc.edu.pe>
2024-10-31 18:26:33 -0500

    feat(Shared): added interfaces.

---
+++ UPC.HomeFlavors.API/Shared/Interfaces/ASP/Configuration/Extensions/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UPC.HomeFlavors.API.Shared.Interfaces.ASP.Configuration.Extensions
+{
+    public static partial class StringExtensions
+    {
+        public static string ToKebabCase(this string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return text;
+            }
+
+            return KebabCaseRegex().Replace(text, "-$1")
+                .Trim()
+                .ToLower();
+        }
+
+        [GeneratedRegex("(?<!^)([A-Z][a-z]|(?<=[a-z])[A-Z])", RegexOptions.Compiled)]
+        private static partial Regex KebabCaseRegex();
+    }
+}
