ba95b970ce4ed6f6f8ba360e883b9f83c5f34a1c
Yair Aru Acevedo <u202125984@upc.edu.pe>
2024-10-31 18:40:37 -0500

    feat(User-Management): added profile aggregates.

---
+++ UPC.HomeFlavors.API/User-Management/Domain/Model/Aggregates/Profile.cs
@@ -0,0 +1,19 @@
+﻿namespace UPC.HomeFlavors.API.Profiles.Domain.Model.Aggregates
+{
+    public class Profile
+    {
+        public int Id { get; }
+        public string Name { get; private set; }
+        public string LastName { get; private set; }
+        public string Email { get; private set; }
+        public string Cellphone { get; private set; }
+
+        public Profile(string name, string lastname, string email, string cellphone)
+        {
+            Name = name;
+            LastName = lastname;
+            Email = email;
+            Cellphone = cellphone;
+        }
+    }
+}
