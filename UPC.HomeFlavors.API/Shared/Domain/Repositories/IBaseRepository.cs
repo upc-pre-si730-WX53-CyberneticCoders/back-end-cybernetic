972d1d8124011ee3f90441595433d4a416fcffd8
Yair Aru Acevedo <u202125984@upc.edu.pe>
2024-10-31 18:23:23 -0500

    feat(Shared): added domain

---
+++ UPC.HomeFlavors.API/Shared/Domain/Repositories/IBaseRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace UPC.HomeFlavors.API.Shared.Domain.Repositories
+{
+    public interface IBaseRepository<TEntity>
+    {
+        Task AddAsync(TEntity entity);
+        Task<TEntity?> FindByIdAsync(int id);
+        void Update(TEntity entity);
+        void Remove(TEntity entity);
+        Task<IEnumerable<TEntity>> ListAsync();
+    }
+}
---
+++ UPC.HomeFlavors.API/Shared/Domain/Repositories/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace UPC.HomeFlavors.API.Shared.Domain.Repositories
+{
+    public interface IUnitOfWork
+    {
+        Task CompleteAsync();
+    }
+}
